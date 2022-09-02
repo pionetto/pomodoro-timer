28325a58779dee56aabb22bc96fce420fffbf1d2
Pio Lima <pio.lima@gmail.com>
2022-09-02 12:10:52 -0400

    Function and button session length

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function App() {
 
     const [displayTime, setDisplayTime] = React.useState(25*60);
+    const [breakTime, setBreakTime] = React.useState(5*60);
+    const [sessionTime, setSessionTime] = React.useState(25*60);
 
     const formatTime = (time) => {
         let minutes = Math.floor(time / 60);
@@ -13,13 +15,28 @@ function App() {
 
     }
 
+    const changeTime = (amount, type) => {
+        if (type == "break") {
+            setBreakTime ((prev) => prev + amount);
+        } else {
+            setSessionTime((prev) => prev + amount);
+        }
+    }
     return ( 
     <div>
         <Length 
             title={"break length"} 
-            changeTime={null} 
+            changeTime={changeTime} 
             type={"break"} 
-            time={null} 
+            time={breakTime} 
+            formatTime={formatTime}
+        />
+
+        <Length 
+            title={"session length"} 
+            changeTime={changeTime} 
+            type={"session"} 
+            time={sessionTime} 
             formatTime={formatTime}
         />
      <h1>{formatTime(displayTime)}</h1>
@@ -32,8 +49,16 @@ function Length({title, changeTime, type, time, formatTime}) {
         <div>
             <h3>{title}</h3>
             <div className="time-sets">
-                <button className="btn-small deep-purple lighten-2"> 
+                <button className="btn-small deep-purple lighten-2"
+                    onClick={() => changeTime(-60, type)}
+                > 
                     <i className="material-icons">arrow_downward</i>
+                </button>
+                <h3>{formatTime(time)}</h3>
+                <button className="btn-small deep-purple lighten-2"
+                onClick={() => changeTime(60, type)}
+                > 
+                    <i className="material-icons">arrow_upward</i>
                 </button>
             </div>
         </div>
