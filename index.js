1bf6a0b12eea3db47fc10fb287a33b06c8c9a0ee
Pio Lima <pio.lima@gmail.com>
2022-09-02 10:58:51 -0400

    Showing converted time on screen

---
+++ index.js
@@ -0,0 +1,23 @@
+function App() {
+
+    const [displayTime, setDisplayTime] = React.useState(25*60);
+
+    const formatTime = (time) => {
+        let minutes = Math.floor(time / 60);
+        let seconds = time % 60;
+        return ( 
+            (minutes < 10 ? "0" + minutes : minutes) +
+            ":" +
+            (seconds < 10 ? "0" + seconds : seconds)
+        );
+
+    }
+
+    return ( 
+    <div>
+     <h1>{formatTime(displayTime)}</h1>
+    </div>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
